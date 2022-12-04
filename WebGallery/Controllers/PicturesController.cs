e31205bc917c62b2a1968b8387efe5567c9c6493
Nagares <theannetko@gmail.com>
2022-12-04 22:30:28 +0200

    added comments to  project

---
+++ WebGallery/Controllers/PicturesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebGallery.Data;
+using WebGallery.Models;
+
+namespace WebGallery.Controllers
+{
+    public class PicturesController : Controller
+    {
+        private readonly WebGalleryContext _context;
+        Picture picture = new Picture() { FilePath = "/", FileName = "*/", LoadDate = DateTime.Now };
+        public PicturesController(WebGalleryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pictures
+        public async Task<IActionResult> Index()
+        {
+
+            ViewBag.Pictures = new SelectList(await _context.Picture.ToListAsync(), "Id", "FileName");
+            return View(picture);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Picture pic)
+        {
+            if (pic.Id == 0) picture = new Picture() { FilePath = "/", FileName = "*/", LoadDate = DateTime.Now };
+            picture = (await _context.Picture.ToListAsync()).FirstOrDefault(p => p.Id == pic.Id);
+            return await Index();
+        }
+
+
+        // GET: Pictures/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pictures/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FileName,FileSize,LoadDate,FilePath")] Picture picture)
+        {
+
+           
+            picture.LoadDate = DateTime.Now;
+            picture.FilePath = "C:\\Users\\Bavovna\\source\\repos\\WebGallery\\WebGallery\\images\\" + picture.FileName; //generating a link to store the path to the image (according to those tasks)
+            picture.FileName = picture.FileName.Split('.')[0];
+            if (ModelState.IsValid)
+            {
+                _context.Add(picture);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(picture);
+        }
+
+       
+
+    }
+}
