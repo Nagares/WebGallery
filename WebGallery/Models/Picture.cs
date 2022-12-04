7e236f810218c65410a26ee48926f1000128bc85
Nagares <theannetko@gmail.com>
2022-12-04 22:30:28 +0200

    Добавьте файлы проекта.

---
+++ WebGallery/Models/Picture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebGallery.Models
+{
+    public class Picture
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public int FileSize { get; set; }
+        public DateTime LoadDate { get; set; }
+        public string FilePath { get; set; }
+
+    }
+
+}
